fbf3b6a3fbac790467b83457e834d6e8ecf2a36d
Alvin Kimata <alvinkimata@gmail.com>
2023-02-20 11:11:59 +0300

    check and validate user credentials

--- Aspnet core Identity/Pages/Register.cshtml.cs
+++ Aspnet core Identity/Pages/Register.cshtml.cs
@@ -1,14 +1,51 @@
 using Aspnet_core_Identity.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace Aspnet_core_Identity.Pages
 {
     public class RegisterModel : PageModel
     {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
         public Register Model { get; set; }
+
+        public RegisterModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        { 
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser(){
+                    UserName = Model.Email,
+                    Email = Model.Email
+                };
+
+                var result = await userManager.CreateAsync(user, Model.Password);
+
+                if (result.Succeeded)
+                {
+                    //Sign in the user.
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToPage("Index");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return Page();
         }
     }
 }
