74a5e09928337d8024cee34f78c98b278fb6cf6a
Alvin Kimata <alvinkimata@gmail.com>
2023-02-20 13:00:40 +0300

    create logout html page

---
+++ Aspnet core Identity/Pages/Logout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Aspnet_core_Identity.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
