b6e8b747a2d5f5f3108eb8d366d62b6437eca132
Alvin Kimata <alvinkimata@gmail.com>
2023-03-08 09:38:34 +0300

    update existing changes

--- Aspnet core Identity/Controllers/AdministratorController.cs
+++ Aspnet core Identity/Controllers/AdministratorController.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Aspnet_core_Identity.Controllers
 {
-    public class AdministratorController
+    public class AdministratorController : Controller
     {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministratorController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
     }
 }
