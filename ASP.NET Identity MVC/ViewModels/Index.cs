8ed5e07e14a971b46b66a3118af1e10914196a9a
Alvin Kimata <alvinkimata@gmail.com>
2023-03-20 12:18:57 +0300

    add index page

---
+++ ASP.NET Identity MVC/ViewModels/Index.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace ASP.NET_Identity_MVC.ViewModels
+{
+    public class Index
+    {
+        private readonly ILogger<Index> _logger;
+
+        public Index(ILogger<Index> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
+
+
+
