153bc04c1d7d23a85180cdaad47abda0a17878e6
Alvin Kimata <alvinkimata@gmail.com>
2023-03-21 09:45:12 +0300

    Implement DBContext class

---
+++ ASP.NET Identity MVC/Models/IdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_Identity_MVC.Models
+{
+    public class IdentityDbContext : DbContext
+    {
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employees> Employees { get; set; }
+    }
+}
