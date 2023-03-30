bd7c7b6ad97496d4e50f5d103e942018534e416b
Alvin Kimata <alvinkimata@gmail.com>
2023-03-30 14:39:53 +0300

    add controller

--- OpenAIImageGenerator/Controllers/HomeController.cs
+++ OpenAIImageGenerator/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using OpenAIImageGenerator.Models;
 using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json.Serialization;
+using static OpenAIImageGenerator.Models.ImageInfo;
 
 namespace OpenAIImageGenerator.Controllers
 {
@@ -27,6 +32,27 @@ namespace OpenAIImageGenerator.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GenerateImage([FromBody] RequiredImage obj)
+        {
+            string imglink = string.Empty;
+            var response = new ResponseModel();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", APIKEY);
+                var Message = await client.PostAsync("https://api.openai.com/v1/images/generations", new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application.json"));
+
+                if (Message.IsSuccessStatusCode)
+                {
+                    var content = await Message.Content.ReadAsStringAsync();
+                    response = JsonConverter.DeserializeObject <ResponseModel>(content);
+                    imglink = response.data[0].url.ToString();
+                }
+            }
+            return Json(response);
         }
     }
 }
