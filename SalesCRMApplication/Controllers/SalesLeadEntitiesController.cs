59b5e2bc3af3c45868936fa98ec80fcf609c7113
Alvin Kimata <alvinkimata@gmail.com>
2023-03-30 10:19:26 +0300

    add SalesLead controller with views

---
+++ SalesCRMApplication/Controllers/SalesLeadEntitiesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SalesCRMApplication.Data;
+using SalesCRMApplication.Models;
+
+namespace SalesCRMApplication.Controllers
+{
+    public class SalesLeadEntitiesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SalesLeadEntitiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SalesLeadEntities
+        public async Task<IActionResult> Index()
+        {
+              return _context.SalesLead != null ? 
+                          View(await _context.SalesLead.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.SalesLead'  is null.");
+        }
+
+        // GET: SalesLeadEntities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.SalesLead == null)
+            {
+                return NotFound();
+            }
+
+            var salesLeadEntity = await _context.SalesLead
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (salesLeadEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(salesLeadEntity);
+        }
+
+        // GET: SalesLeadEntities/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SalesLeadEntities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Mobile,Email,Source")] SalesLeadEntity salesLeadEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(salesLeadEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salesLeadEntity);
+        }
+
+        // GET: SalesLeadEntities/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.SalesLead == null)
+            {
+                return NotFound();
+            }
+
+            var salesLeadEntity = await _context.SalesLead.FindAsync(id);
+            if (salesLeadEntity == null)
+            {
+                return NotFound();
+            }
+            return View(salesLeadEntity);
+        }
+
+        // POST: SalesLeadEntities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Mobile,Email,Source")] SalesLeadEntity salesLeadEntity)
+        {
+            if (id != salesLeadEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(salesLeadEntity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SalesLeadEntityExists(salesLeadEntity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salesLeadEntity);
+        }
+
+        // GET: SalesLeadEntities/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.SalesLead == null)
+            {
+                return NotFound();
+            }
+
+            var salesLeadEntity = await _context.SalesLead
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (salesLeadEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(salesLeadEntity);
+        }
+
+        // POST: SalesLeadEntities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.SalesLead == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.SalesLead'  is null.");
+            }
+            var salesLeadEntity = await _context.SalesLead.FindAsync(id);
+            if (salesLeadEntity != null)
+            {
+                _context.SalesLead.Remove(salesLeadEntity);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SalesLeadEntityExists(int id)
+        {
+          return (_context.SalesLead?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
