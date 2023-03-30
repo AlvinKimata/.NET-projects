c075ef0b9a0b3ad16021207eecfc9100f1ff0a69
Alvin Kimata <alvinkimata@gmail.com>
2023-03-30 14:25:38 +0300

    setup project and add model

---
+++ OpenAIImageGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OpenAIImageGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ OpenAIImageGenerator/Models/ImageInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace OpenAIImageGenerator.Models
+{
+    public class ImageInfo
+    {
+        public string ? ImageText { get; set; }
+        public class RequiredImage
+        {
+            public string? prompt { get; set; }
+            public short? n { get; set; }
+            public string ? size { get; set; }
+        }
+
+        public class ImageUrls
+        {
+            public string? url { get; set;}
+        }
+
+        //Response handling.
+        public class ResponseModel
+        {
+            public long created { get; set; }
+            public List < ImageUrls > ? data { get; set; }
+        }
+
+    }
+}
