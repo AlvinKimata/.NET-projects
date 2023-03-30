2786763e4a9c52edc8639195b07355a22da6b818
Alvin Kimata <alvinkimata@gmail.com>
2023-03-30 09:45:33 +0300

    run migration

--- SalesCRMApplication/Program.cs
+++ SalesCRMApplication/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SalesCRM")));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SalesCRMConnection")));
 
 var app = builder.Build();
 
