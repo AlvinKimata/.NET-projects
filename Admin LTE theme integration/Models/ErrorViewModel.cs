bbf3f14572041d0bc596f1f181ba95757bb7a17b
Alvin Kimata <alvinkimata@gmail.com>
2023-03-31 15:16:49 +0300

    Setup admin theme project

---
+++ Admin LTE theme integration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Admin_LTE_theme_integration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
