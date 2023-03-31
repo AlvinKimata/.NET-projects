8dc31b3c0ce5509265a3f9a1192df6bbc95c2d29
Alvin Kimata <alvinkimata@gmail.com>
2023-03-31 15:48:49 +0300

    perform migration

---
+++ Admin LTE theme integration/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Admin_LTE_theme_integration.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Admin_LTE_theme_integration.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options): base(options) 
+        { 
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
--- Admin LTE theme integration/Program.cs
+++ Admin LTE theme integration/Program.cs
@@ -1,7 +1,11 @@
+using Admin_LTE_theme_integration.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("StudentAdmin")));
 
 var app = builder.Build();
 
