1ec4504f174159459c782eb46ecd8dc4ff827344
Alvin Kimata <alvinkimata@gmail.com>
2023-03-31 15:31:59 +0300

    implement student model and setup connectionstring

---
+++ Admin LTE theme integration/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Admin_LTE_theme_integration.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public int Marks { get; set; }
+    }
+}
