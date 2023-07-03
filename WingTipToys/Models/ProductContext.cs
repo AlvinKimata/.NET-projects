5047d4cbcd757eb74316ef75dcf3bf59d6971c10
Alvin Kimata <alvinkimata@gmail.com>
2023-07-03 13:01:13 +0300

    add category, products classes and seed dbase with initial data

---
+++ WingTipToys/Models/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WingTipToys.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("WingTipToys")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
