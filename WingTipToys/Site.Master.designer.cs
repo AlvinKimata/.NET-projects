b6572dc978f47d4ebaa92928cedf939681fc1af1
Alvin Kimata <alvinkimata@gmail.com>
2023-07-03 15:42:15 +0300

    Add ListView Control to display all categories

--- WingTipToys/Site.Master.designer.cs
+++ WingTipToys/Site.Master.designer.cs
@@ -24,6 +24,15 @@ namespace WingTipToys
         protected global::System.Web.UI.WebControls.Image Image1;
 
         /// <summary>
+        /// categoryList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView categoryList;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
