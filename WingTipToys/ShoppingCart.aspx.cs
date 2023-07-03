31206fd51394d7132151bf9254b976add89d65ce
Alvin Kimata <alvinkimata@gmail.com>
2023-07-03 20:14:59 +0300

    Updating and Removing Shopping Cart Items

--- WingTipToys/ShoppingCart.aspx.cs
+++ WingTipToys/ShoppingCart.aspx.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using WingTipToys.Models;
+using System.Collections.Specialized;
+using System.Collections;
+using System.Web.ModelBinding;
 
 namespace WingTipToys
 {
@@ -26,6 +29,7 @@ namespace WingTipToys
                     LabelTotalText.Text = "";
                     lblTotal.Text = "";
                     ShoppingCartTitle.InnerText = "Shopping Cart is Empty";
+                    UpdateBtn.Visible = false;
                 }
             }
 
@@ -34,6 +38,51 @@ namespace WingTipToys
         {
             ShoppingCartActions actions = new ShoppingCartActions();
             return actions.GetCartItems();
+        }
+        public List<CartItem> UpdateCartItems()
+        {
+            using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+            {
+                String cartId = usersShoppingCart.GetCartId();
+                ShoppingCartACtions.ShoppingCartUpdates[] cartUpdates = new ShoppingCartACtions.ShoppingCartUpdates[CartList.Rows.Count];
+                for (int i = 0; i < CartList.Rows.Count; i++)
+                {
+                    IOrderedDictionary rowValues = new OrderedDictionary();
+                    rowValues = GetValues(CartList.Rows[i]);
+                    cartUpdates[i].ProductId = Convert.ToInt32(rowValues["ProductID"]);
+
+                    CheckBox cbRemove = new CheckBox();
+                    cbRemove = (CheckBox)CartList.Rows[i].FindControl("Remove");
+                    cartUpdates[i].RemoveItem = cbRemove.Checked;
+
+                    TextBox quantityTextBox = new TextBox();
+                    quantityTextBox = (TextBox)CartList.Rows[i].FindControl("PurchaseQunatity");
+                    cartUpdates[i].PurchaseQuantity = Convert.ToInt16(quantityTextBox.Text.ToString());
+                }
+                usersShoppingCart.UpdateShoppingCartDatabase(cartId, cartUpdates);
+                CartList.DataBind();
+                lblTotal.Text = String.Format("{0:c}", usersShoppingCart.GetTotal());
+                return usersShoppingCart.GetCartItems();
+            }
+        }
+        public static IOrderedDictionary GetValues(GridView row)
+        {
+            IOrderedDictionary values = new OrderedDictionary();
+            foreach (DataControlFieldCell cell in row.Cells)
+            {
+                if (cell.Visible)
+                {
+                    //Extract values from the cell.
+                    cell.ContainingField.ExtractValuesFromCell(values, cell, row.RowState, true);
+                }
+                return values;
+            }
+
+            protected void UpdateBtn_Click(object sender, EventArgs e)
+            {
+                UpdateCartItems();
+            }
+
         }
     }
 }
