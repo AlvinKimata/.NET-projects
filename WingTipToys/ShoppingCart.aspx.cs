9db19b31e29a49d6739226ede154fae89e23556d
Alvin Kimata <alvinkimata@gmail.com>
2023-07-04 17:52:36 +0300

    Add the CheckOutBtn_Click event handler

--- WingTipToys/ShoppingCart.aspx.cs
+++ WingTipToys/ShoppingCart.aspx.cs
@@ -85,5 +85,14 @@ namespace WingTipToys
         {
             UpdateCartItems();
         }
+
+        protected void CheckoutBtn_Click(object sender, ImageClickEventArgs e)
+        {
+            using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+            {
+                Session["payment_amt"] = usersShoppingCart.GetTotal();
+            }
+            Response.Redirect("Checkout/CheckoutStart.aspx");
+        }
     }
 }
