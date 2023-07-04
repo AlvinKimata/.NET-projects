69c6f351ca2c7558284483d841e6a6816d3b0148
Alvin Kimata <alvinkimata@gmail.com>
2023-07-04 17:46:15 +0300

    Add PayPal Class and API Credentials

--- WingTipToys/Logic/PayPalFunctions.cs
+++ WingTipToys/Logic/PayPalFunctions.cs
@@ -196,4 +196,133 @@ public class NVPAPICaller
             return false;
         }
     }
+    public string HttpCall(string NvpRequest)
+    {
+        string url = pEndPointURL;
+        string strPost = NvpRequest + "&" + buildCredentialsNVPString();
+        strPost = strPost + "&BUTTONSOURCE=" + HttpUtility.UrlEncode(BNCode);
+
+        HttpWebRequest objRequest = (HttpRequest)WebRequest.Create(url);
+        objRequest.Timeout = Timeout;
+        objRequest.Method = "POST";
+        objRequest.ContentLength = strPost.Length;
+
+        try
+        {
+            using (StreamWriter myWriter = new StreamWriter(objRequest.GetRequestStream()))
+            {
+                myWriter.Write(strPost);
+            }
+        }
+        catch (Exception)
+        {
+            //Log exceptions.
+        }
+
+        //Retrieve the response returned from the NVP API call to PayPal.
+        HttpWebResponse objResponse = (HttpWebResponse)objRequest.GetResponse();
+        string result;
+        using (StreamReader sr  = new StreamReader(objResponse.GetResponseStream()))
+        {
+            result = sr.ReadToEnd();
+        }
+        return result;
+    }
+    private string buildCredentialsNVPString()
+    {
+        NVPCodec codec = new NVPCodec();
+
+        if (!IsEmpty(APIUsername))
+            codec["USER"] = APIUsername;
+        if (!IsEmpty(APIPassword))
+            codec[PWD] = APIPassword;
+
+        if (!IsEmpty(APISignature))
+            codec[SIGNATURE] = APISignature;
+
+        if (!IsEmpty(Subject))
+            codec["SUBJECT"] = Subject;
+
+        codec["VERSION"] = "88.0";
+
+        return codec.Encode();
+    }
+
+    public static bool IsEmpty(string s)
+    {
+        return s == null || s.Trim() == string.Empty;
+    }
+}
+
+
+public sealed class NVPCodec : NameValueCollection
+{
+    private const string AMPERSAND = "&";
+    private const string EQUALS = "=";
+    private static readonly char[] AMPERSAND_CHAR_ARRAY = AMPERSAND.ToCharArray();
+    private static readonly char[] EQUALS_CHAR_ARRAY = EQUALS.ToCharArray();
+
+    public string Encode()
+    {
+        StringBuilder sb = new StringBuilder();
+        bool firstPair = true;
+        foreach (string kv in AllKeys)
+        {
+            string name = HttpUtility.UrlEncode(kv);
+            string value = HttpUtility.UrlEncode(this[kv]);
+            if (!firstPair)
+            {
+                sb.Append(AMPERSAND);
+            }
+            sb.Append(name).Append(EQUALS).Append(value);
+            firstPair = false;
+        }
+        return sb.ToString();
+    }
+
+    public void Decode(string nvpstring)
+    {
+        Clear();
+        foreach (string nvp in nvpstring.Split(AMPERSAND_CHAR_ARRAY))
+        {
+            string[] tokens = nvp.Split(EQUALS_CHAR_ARRAY);
+            if (tokens.Length >= 2)
+            {
+                string name = HttpUtility.UrlDecode(tokens[0]);
+                string value = HttpUtility.UrlDecode(tokens[1]);
+                Add(name, value);
+            }
+        }
+    }
+
+    public void Add(string name, string value, int index)
+    {
+        this.Add(GetArrayName(index, name), value);
+    }
+
+    public void Remove(string arrayName, int index)
+    {
+        this.Remove(GetArrayName(index, arrayName));
+    }
+
+    public string this[string name, int index]
+    {
+        get
+        {
+            return this[GetArrayName(index, name)];
+        }
+        set
+        {
+            this[GetArrayName(index, name)] = value;
+        }
+    }
+
+    private static string GetArrayName(int index, string name)
+    {
+        if (index < 0)
+        {
+            throw new ArgumentOutOfRangeException("index", "index cannot be negative : " + index);
+        }
+        return name + index;
+    }
 }
