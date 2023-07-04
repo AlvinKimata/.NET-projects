47ae12af029a7029ba311ff08ddfcc4028de5b95
Alvin Kimata <alvinkimata@gmail.com>
2023-07-04 17:57:53 +0300

    Send Purchase Details to PayPal

--- WingTipToys/Checkout/CheckoutStart.aspx.cs
+++ WingTipToys/Checkout/CheckoutStart.aspx.cs
@@ -11,7 +11,28 @@ namespace WingTipToys.Checkout
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            NVPAPICaller payPalCaller = new NVPAPICaller();
+            string retMsg = "";
+            string token = "";
 
+            if (Session["payment-amt"] != null)
+            {
+                string amt = Session["payment_amt"].ToString();
+                bool ret = payPalCaller.ShortcutExpressCheckout(amt, ref token, ref retMsg);
+                if (ret)
+                {
+                    Session["token"] = token;
+                    Response.Redirect(retMsg);
+                }
+                else
+                {
+                    Response.Redirect("CheckoutError.aspx?" + retMsg);
+                }
+            }
+            else
+            {
+                Response.Redirect("CheckoutError.aspx?ErrorCode=AmtMissing");
+            }
         }
     }
 }
